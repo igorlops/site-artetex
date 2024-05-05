134c820f7cf06573e54720386671ef6d75f8bac9
igorlops <claudioigorai@gmail.com>
2024-05-05 09:33:21 -0300

    Criação das páginas politica de privacidade, termos de uso e contato

---
+++ resources/views/police/politica.blade.php
@@ -0,0 +1,42 @@
+@extends('layout.app')
+
+@section('title-site', "Politica de privacidade")
+
+@section('content')
+<div class="container">
+    <h2>Política de Privacidade de Dados</h2>
+    <p>A Artetex se compromete com a segurança dos seus dados e mantém suas informações em absoluto sigilo!</p>
+    <p>Priorizamos a privacidade e a segurança de nossos clientes durante todo o processo de navegação e compra pelo site. Todos os dados cadastrados, como nome, endereço e CPF, nunca serão comercializados ou trocados. Alguns dados necessários para empresas de logística e meios de pagamento realizarem a cobrança e envio de seu pedido serão divulgados para terceiros, quando essas informações forem essenciais para o processo de entrega e cobrança. Seus dados pessoais são fundamentais para que seu pedido chegue em segurança.</p>
+    <p>Utilizamos cookies e informações de sua navegação para traçar um perfil do público que visita o site e aprimorar nossos serviços, produtos e conteúdos, conforme regulamentado pela Lei Geral de Proteção de Dados (LGPD). Durante todo esse processo, mantemos suas informações em sigilo absoluto.</p>
+    <h4><strong>O que é a LGPD?</strong></h4>
+    <p>A Lei Geral de Proteção de Dados (Lei nº 13.709/2018) regulamenta o tratamento de dados pessoais de clientes e usuários por parte de empresas públicas e privadas.</p>
+    <h4><strong>O que são dados pessoais?</strong></h4>
+    <p>Dados pessoais são quaisquer informações capazes de identificar você e/ou qualquer pessoa física. Isso inclui não apenas informações imediatas, como nome e CPF, mas também dados que, em conjunto, tornam uma pessoa identificável.</p>
+    <h4><strong>O que é tratamento de dados?</strong></h4>
+    <p>É o termo utilizado pela LGPD que resume tudo o que uma empresa pode fazer com dados pessoais a que tem acesso, como coleta, qualificação, compartilhamento e exclusão.</p>
+    <h4><strong>O que são cookies?</strong></h4>
+    <p>Cookies são pequenos arquivos de texto enviados para seu navegador que registram seu comportamento ao acessar um site, sem armazenar dados pessoais. Utilizamos cookies, pixels e outras tecnologias para reconhecer seu navegador ou dispositivo, aprender mais sobre seus interesses e melhorar sua experiência de navegação.</p>
+    <h4><strong>Cookies da Sessão</strong></h4>
+    <p>Cookies da sessão são temporários e utilizados para lembrar você durante sua visita ao site, perdendo a validade ao fechar o navegador.</p>
+    <h4><strong>Cookies Persistentes</strong></h4>
+    <p>Cookies persistentes são utilizados para medir a eficácia do site e lembrar suas preferências, permanecendo mesmo após fechar o navegador. Eles nos ajudam a analisar o comportamento do usuário e melhorar a funcionalidade do site.</p>
+    <h4><strong>Como Usamos os Dados Pessoais que Coletamos?</strong></h4>
+    <p>Seus dados pessoais são utilizados apenas para o propósito que motivou o cadastro e não são divulgados a terceiros, exceto no cumprimento de ordens judiciais ou por autoridades públicas.</p>
+    <h4><strong>Por Quanto Tempo Usamos os Dados Pessoais que Coletamos?</strong></h4>
+    <p>Os dados pessoais são armazenados apenas pelo tempo necessário para cumprir com as finalidades para as quais foram coletados, salvo obrigação legal em contrário.</p>
+    <h4><strong>Quais são Seus Direitos?</strong></h4>
+    <p>Você tem o direito de solicitar informações referentes ao tratamento de seus dados pessoais e de realizar pedidos como confirmação, correção, anonimização, portabilidade e revogação do consentimento. Para isso, entre em contato conosco por meio das informações disponibilizadas.</p>
+    <p>Cadastre-se e compre com tranquilidade, sem preocupações.</p>
+    <p>As alterações sobre nossa política de privacidade serão devidamente informadas neste espaço.</p>
+    <p>A Artetex garante que utiliza seus dados pessoais apenas para processar os pedidos realizados e que as informações são protegidas durante todo o processo de compra. Verifique abaixo os nossos certificados de segurança.</p>
+
+    <h4><strong>Certificados de Segurança</strong></h4>
+    <p>CERTIFICADO SSL: O certificado digital SSL é o nível de segurança mais alto e obrigatório para todos os sites que realizam transmissão de dados,
+        como, por exemplo, nos e-commerces que solicitam informações sensíveis aos clientes, como e-mail e dados do cartão de crédito.
+        Esse certificado pode ser verificado pelo cadeado verde na barra de navegação que atesta que o site protege as informações que
+        são enviadas pelos clientes por meio de criptografia de dados, o que praticamente elimina a possibilidade de interceptação de dados.
+        Sites seguros são acessados pelo prefixo HTTPS, os sites que possuem acesso apenas pelo prefixo HTTP não são seguros e não criptografam as informações.
+        Confira o cadeado verde e o prefixo HTTPS no seu navegador e verifique o nosso selo de Certificado SSL no rodapé da página.</p>
+</div>
+
+@endsection
---
+++ resources/views/police/termos.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.app')
+
+@section('title-site', "Termos de uso")
+
+@section('content')
+    <div class="container">
+        <h2>Termos de Uso</h2>
+        <p><strong>1. Termos</strong></p>
+        <p>Ao acessar o site Artetex, você concorda em cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis e concorda que é responsável pelo cumprimento de todas as leis locais aplicáveis. Se você não concordar com algum desses termos, está proibido de usar ou acessar este site. Os materiais contidos neste site são protegidos pelas leis de direitos autorais e marcas comerciais aplicáveis.</p>
+
+        <p><strong>2. Uso de Licença</strong></p>
+        <p>É concedida permissão para baixar temporariamente uma cópia dos materiais (informações ou software) no site, apenas para visualização transitória pessoal e não comercial. Esta é a concessão de uma licença, não uma transferência de título e, sob esta licença, você não pode:</p>
+        <ol>
+            <li>modificar ou copiar os materiais;</li>
+            <li>usar os materiais para qualquer finalidade comercial ou para exibição pública (comercial ou não comercial);</li>
+            <li>tentar descompilar ou fazer engenharia reversa de qualquer software contido no site;</li>
+            <li>remover quaisquer direitos autorais ou outras notações de propriedade dos materiais; ou</li>
+            <li>transferir os materiais para outra pessoa ou ‘espelhar’ os materiais em qualquer outro servidor.</li>
+        </ol>
+        <p>Esta licença será automaticamente rescindida se você violar alguma dessas restrições e poderá ser rescindida a qualquer momento. Ao encerrar a visualização desses materiais ou após o término desta licença, você deve apagar todos os materiais baixados em sua posse, seja em formato eletrônico ou impresso.</p>
+
+        <p><strong>3. Isenção de Responsabilidade</strong></p>
+        <p>Os materiais no site da Artetex são fornecidos ‘como estão’. Não oferecemos garantias, expressas ou implícitas, e, por este meio, isentamos e negamos todas as outras garantias, incluindo, sem limitação, garantias implícitas ou condições de comercialização, adequação a um fim específico ou não violação de propriedade intelectual, ou outra violação de direitos.</p>
+        <p>Além disso, não garantimos ou fazemos qualquer representação relativa à precisão, aos resultados prováveis ou à confiabilidade do uso dos materiais em nosso site, ou de outra forma relacionado a esses materiais, ou em sites vinculados a este site.</p>
+
+        <p><strong>4. Limitações</strong></p>
+        <p>Em nenhum caso, a Artetex ou seus fornecedores serão responsáveis por quaisquer danos (incluindo, sem limitação, danos por perda de dados ou lucro, ou devido à interrupção dos negócios) decorrentes do uso ou da incapacidade de usar os materiais do site, mesmo que um representante autorizado da Artetex tenha sido notificado oralmente ou por escrito da possibilidade de tais danos. Como algumas jurisdições não permitem limitações em garantias implícitas ou limitações de responsabilidade por danos consequentes ou incidentais, essas limitações podem não se aplicar a você.</p>
+
+        <p><strong>5. Precisão dos Materiais</strong></p>
+        <p>Os materiais exibidos no site da Artetex podem incluir erros técnicos, tipográficos ou fotográficos. Não garantimos que qualquer material em nosso site seja preciso, completo ou atual. A Artetex pode fazer alterações nos materiais contidos em seu site a qualquer momento, sem aviso prévio. No entanto, não nos comprometemos a atualizar os materiais.</p>
+
+        <p><strong>6. Links</strong></p>
+        <p>A Artetex não analisou todos os sites vinculados ao seu site e não é responsável pelo conteúdo de nenhum site vinculado. A inclusão de qualquer link não implica endosso pela Artetex do site. O uso de qualquer site vinculado é por conta e risco do usuário.</p>
+
+        <p><strong>7. Modificações</strong></p>
+        <p>A Artetex pode revisar estes termos de serviço do site a qualquer momento, sem aviso prévio. Ao usar este site, você concorda em ficar vinculado à versão atual destes termos de serviço.</p>
+
+        <p><strong>8. Lei Aplicável</strong></p>
+        <p>Estes termos e condições são regidos e interpretados de acordo com as leis da Artetex e você se submete irrevogavelmente à jurisdição exclusiva dos tribunais naquele estado ou localidade.</p>
+    </div>
+@endsection
