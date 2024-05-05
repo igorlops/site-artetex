0ee714591c3bd1dcd74779c335b82e2d7c5b071e
igorlops <claudioigorai@gmail.com>
2024-05-05 14:04:06 -0300

    pagina do sobre e tecidos

--- resources/views/sobre/index.blade.php
+++ resources/views/sobre/index.blade.php
@@ -4,32 +4,46 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="d-flex justify-content-around flex-column gap-3 ">
-            <div class="col-4 d-flex flex-row w-100">
-                <div class="text col-6">
-                    <h2>Com uma história de tradição e profunda expertise no setor têxtil</h2>
-                    <p>A Artetex se destaca como uma referência na comercialização de tecidos e malhas,
-                        oferecendo produtos da mais alta qualidade para atender às demandas exigentes do mercado de moda brasileiro.</p>
-                </div>
-                <div class="image col-6">
-                    <img class="h-100"  src="{{asset("img/photo-sobre-nos.jpg")}}" alt="Conhecida por sua tradição e experiência no mercado têxtil">
+        <div  class="sobre">
+            <h2>Sobre Nós</h2>
+        </div>
+
+    <body>
+        <div class="container">
+            <div class="d-flex justify-content-around flex-column gap-3">
+                <div class="primeiro-topico col-4 d-flex flex-row w-100 align-items-center">
+                    <div class="text-primery col-6">
+                        <h2>Com uma história de tradição e profunda expertise no setor têxtil</h2>
+                        <p>A Artetex se destaca como uma referência na comercialização de tecidos e malhas,
+                            oferecendo produtos da mais alta qualidade para atender às demandas exigentes do mercado de moda brasileiro.</p>
+                    </div>
+                    <div class="image col-6">
+                        <img class="h-100" src="{{asset("img/photo-sobre-nos.jpg")}}" alt="Conhecida por sua tradição e experiência no mercado têxtil">
+                    </div>
                 </div>
             </div>
-
-            <div class="col-4 d-flex flex-row w-100">
-                <div class="image col-6">
-                    <img class="h-100" src="{{asset("img/photo-sobre-nos-dois.jpg")}}" alt="Conhecida por sua tradição e experiência no mercado têxtil">
-                </div>
-                <div class="text col-6">
-                    <h2>Equipe estruturada e qualificada</h2>
-                    <p>Possuímos uma equipe estruturada e qualificada na condução do trabalho,
-                        onde nossa prioridade é a entrega de um produto único e de qualidade Artetex.</p>
+        </div>
+        <div class="segundo-topico col-4 d-flex flex-row w-100 align-items-center bg-earth">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 d-flex align-items-center">
+                        <div class="image">
+                            <img class="h-100" src="{{asset("img/photo-sobre-nos-dois.jpg")}}" alt="Conhecida por sua tradição e experiência no mercado têxtil">
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-flex align-items-center">
+                        <div class="text-section">
+                            <h2>Equipe estruturada e qualificada</h2>
+                            <p>Possuímos uma equipe estruturada e qualificada na condução do trabalho,
+                                onde nossa prioridade é a entrega de um produto único e de qualidade Artetex.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div class="col-4 d-flex flex-row w-100">
-                <div class="text col-6">
+        </div>
+        <div class="container">
+            <div class="terceiro-topico col-4 d-flex flex-row w-100 align-items-center">
+                <div class="text-terction col-6">
                     <h2>Aprimoramento, a inovação e a superioridade</h2>
                     <p>Buscamos continuamente o aprimoramento, a inovação e a
                         superioridade do nosso amplo portfólio de tecidos e malhas a fim de
@@ -40,5 +54,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </body>
+
+
 @endsection
--- resources/views/tecidos/index.blade.php
+++ resources/views/tecidos/index.blade.php
@@ -5,26 +5,63 @@
 @section('content')
 
 
-<div class="container">
-    <h1>Lista de Tecidos</h1>
-    <div class="tecidos-list">
-      <div class="tecido-item">
-        <h2>Viscolinho</h2>
-        <a href="#" class="btn btn-primary fale-conosco" id="fale-conosco-1">
-          <span class="text">Fale Conosco</span>
-          <img src="img/icon.png" class="icon" alt="Fale Conosco">
-        </a>
-      </div>
-      <div class="tecido-item">
-        <h2>Linho Rústico</h2>
-        <a href="#" class="btn btn-primary fale-conosco" id="fale-conosco-2">
-          <span class="text">Fale Conosco</span>
-          <img src="img/icon.png" class="icon" alt="Fale Conosco">
-        </a>
-      </div>
-      <!-- Adicione mais itens da lista de tecidos aqui -->
-    </div>
-  </div>
+        <div  class="tecidos">
+            <h2>Tecidos</h2>
+        </div>
+
+        <div class="tecido-pagina container">
+            <div class="item" id="item1">
+                <img src="{{asset("img/tecido01.jpg")}}" alt="Imagem 1">
+                <div class="overlay">
+                    <h3>Título 1</h3>
+                    <p>Descrição 1</p>
+                    <a href="https://api.whatsapp.com/send?phone=SEUNUMERO">Fale Conosco</a>
+                </div>
+            </div>
+            <div class="item" id="item2">
+                <img src="{{asset("img/tecido02.jpg")}}" alt="Imagem 2">
+                <div class="overlay">
+                    <h3>Título 2</h3>
+                    <p>Descrição 2</p>
+                    <a href="https://api.whatsapp.com/send?phone=SEUNUMERO">Fale Conosco</a>
+                </div>
+            </div>
+            <div class="item" id="item3">
+                <img src="{{asset("img/tecido03.jpg")}}" alt="Imagem 3">
+                <div class="overlay">
+                    <h3>Título 3</h3>
+                    <p>Descrição 3</p>
+                    <a href="https://api.whatsapp.com/send?phone=SEUNUMERO">Fale Conosco</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="tecido-pagina container">
+            <div class="item" id="item1">
+                <img src="{{asset("img/tecido01.jpg")}}" alt="Imagem 1">
+                <div class="overlay">
+                    <h3>Título 1</h3>
+                    <p>Descrição 1</p>
+                    <a href="https://api.whatsapp.com/send?phone=SEUNUMERO">Fale Conosco</a>
+                </div>
+            </div>
+            <div class="item" id="item2">
+                <img src="{{asset("img/tecido02.jpg")}}" alt="Imagem 2">
+                <div class="overlay">
+                    <h3>Título 2</h3>
+                    <p>Descrição 2</p>
+                    <a href="https://api.whatsapp.com/send?phone=SEUNUMERO">Fale Conosco</a>
+                </div>
+            </div>
+            <div class="item" id="item3">
+                <img src="{{asset("img/tecido03.jpg")}}" alt="Imagem 3">
+                <div class="overlay">
+                    <h3>Título 3</h3>
+                    <p>Descrição 3</p>
+                    <a href="https://api.whatsapp.com/send?phone=SEUNUMERO">Fale Conosco</a>
+                </div>
+            </div>
+        </div>
 
 
 
